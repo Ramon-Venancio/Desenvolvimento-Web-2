e6af5e2282915f5cc5a3c67183fe7dff577da3c3
Ramon-Venancio <ramonvenanciosousa@gmail.com>
2023-02-28 16:57:26 -0300

    alterações

--- sgt/hello/views.py
+++ sgt/hello/views.py
@@ -5,6 +5,10 @@ def hello(request):
     return HttpResponse('Hello, World')
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name}!")
+    return HttpResponse(f"Olá, {name.capitalize()}!")
 
+def html(request, name):
+    return render(request, 'hello/index.html', {
+        'name':name.capitalize()
+    })
 # Create your views here.
